4a71f5b098e5ad53e7bf239f8a9828fa81c6607a
Srishti Gupta <gsrishti747@gmail.com>
2026-03-15 11:52:57 +0530

    Home Annimation updates

--- src/pages/Agent.jsx
+++ src/pages/Agent.jsx
@@ -1,11 +1,69 @@
-import React from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import React, { useRef } from "react";
 
 const Agent = () => {
+
+  gsap.registerPlugin(ScrollTrigger)
+
+  const imgDivref = useRef(null)
+  const imageRef = useRef(null)
+   const imageArray = [
+    'https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/Olivier_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/Lawrence_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/HugoJoseph_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/ChantalG_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/MyleneS_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/SophieA_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/Claire_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/Michele_480X640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/MEL_480X640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/CAMILLE_480X640_2-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/MAXIME_480X640_2-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/MEGGIE_480X640_2-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/joel_480X640_3-480x640.jpg',
+  ]
+
+  // const imageArray = [
+  //  '<img src="images/Agent1.jpg" alt="agent1" />',
+  //   '<img src="images/Agent2.jpg" alt="agent1" />',
+  //   '<img src="images/Agent3.jpg" alt="agent1" />',
+  //   '<img src="images/Agent4.jpg" alt="agent1" />',
+  //   '<img src="images/Agent5.jpg" alt="agent1" />',
+  //   '<img src="images/Agent6.jpg" alt="agent1" />',
+  //   '<img src="images/Agent7.jpg" alt="agent1" />',
+  //   '<img src="images/Agent8.jpg" alt="agent1" />'
+  // ]
+ 
+
+  useGSAP(function(){
+    gsap.to(imgDivref.current,{
+      scrollTrigger:{
+        trigger:imgDivref.current,
+        start:'top 29%',
+        end:'top -70%',
+        scrub:true,
+        pin :true,
+        onUpdate:(elem)=>{
+          let imageIndex ;
+          if(elem.progress<1){
+            imageIndex = Math.floor(elem.progress*imageArray.length);
+          }else{
+            imageIndex = imageArray.length-1;
+          }
+          imageRef.current.src= imageArray[imageIndex]
+        }
+      }
+    })
+  })
+
   return (
     <div>
       <div className="section1 h-screen">
-      <div className="absolute h-[20vw] w-[15vw] top-50 left-120 rounded-3xl bg-red-50">
-        <img src="images/Agent1.jpg" alt="agent1" />
+      <div ref={imgDivref} className="absolute h-[20vw] w-[15vw] top-50 left-120 rounded-3xl bg-red-50">
+        <img ref={imageRef} src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg" alt="agent1" />
       </div>
 
       <div className="font-[font2] relative">
@@ -16,7 +74,7 @@ const Agent = () => {
       </div>
 
       <div className="pl-[40%] mt-20">
-        <div className="text-6xl ">
+        <div className="text-5xl ">
          &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam laboriosam necessitatibus, id aliquam, ullam earum fugiat corporis porro consequatur excepturi saepe officiis nam atque! Explicabo tempore accusantium ex veritatis nemo? Sed asperiores cum ullam dicta?
         </div>
       </div>
