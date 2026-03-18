396fe8fe8dc1a4c61103dad43d9e6cc6714d3184
Srishti Gupta <gsrishti747@gmail.com>
2026-03-18 22:31:45 +0530

    Navbar changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Agent from './pages/Agent'
 import Project from './pages/Project'
+import Navbar from "./components/nav/Navbar"
+import FullScreenNav from "./components/nav/FullScreenNav"
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
   return (
     <div className="text-white">
 
+      <Navbar/>
+      <FullScreenNav/>
        <Routes >
            <Route path="/" element={<Home/>} />
            <Route path='/agent' element={<Agent/>} />
